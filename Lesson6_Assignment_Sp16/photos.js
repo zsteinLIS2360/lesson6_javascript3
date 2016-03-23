ae78eeda793efab451ae56a430b2caaf8e8c3be5
zachstein99 <zachary.stein99@gmail.com>
2016-03-23 18:10:06 +0000

    lesson 6 first commit

---
+++ Lesson6_Assignment_Sp16/photos.js
@@ -0,0 +1,67 @@
+/*    
+    Program Name:  Photo Gallery Application
+    Author: 
+    Date:   
+    Filename: photos.js
+ */
+
+"use strict"; // interpret document contents in JavaScript strict mode
+
+/* global variables */
+var photoOrder = [1,2,3,4,5];
+var figureCount = 3;
+
+
+
+
+
+
+/* shift all images one figure to the right, and change values in photoOrder array to match  */
+function rightArrow() {
+   for (var i = 0; i < 5; i++) {
+      if ((photoOrder[i] + 1) === 6) {
+         photoOrder[i] = 1;
+      } else {
+         photoOrder[i] += 1;
+      }
+      populateFigures();
+   }//end of for loop
+}//end of rightArrow Function
+
+
+
+
+
+/* shift all images one figure to the left, and change values in photoOrder array to match  */
+function leftArrow() {
+   for (var i = 0; i < 5; i++) {
+      if ((photoOrder[i] - 1) === 0) {
+         photoOrder[i] = 5;
+      } else {
+         photoOrder[i] -= 1;
+      }
+      populateFigures();
+   }//end of for loop
+}//end of leftArrow Function
+
+
+
+
+
+/* create event listeners and populate image elements */
+function setUpPage() {
+   createEventListeners();
+   populateFigures();
+}//end of setUpPage Function
+
+
+
+
+
+
+/* run setUpPage() function when page finishes loading */
+if (window.addEventListener) {
+  window.addEventListener("load", setUpPage, false); 
+} else if (window.attachEvent)  {
+  window.attachEvent("onload", setUpPage);
+}
